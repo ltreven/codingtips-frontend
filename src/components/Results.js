b74ebc4450de9562153f8573d51c950ccf2b0fb6
ltreven <ltreven@gmail.com>
2020-04-10 02:34:45 +0200

    search

--- src/components/Results.js
+++ src/components/Results.js
@@ -11,6 +11,14 @@ class Results extends Component {
             || ''
         };
     }
+
+    componentDidUpdate(prevProps) {
+        const search = (new URLSearchParams(window.location.search)).get('search')
+            || ''
+        if (this.state.search !== search) {
+            this.setState({search: search});
+        }
+    }
     
     render () {
         return (
